107d692af13d93006ca7a3e71e5961e7954a3b03
tumble <tumblegamer@gmail.com>
2022-05-05 16:42:02 +0100

    remove sample achivements

--- bc-achieve.user.js
+++ bc-achieve.user.js
@@ -103,24 +103,4 @@
 	}
 
 	uWindow.BCAchieve = BCAchieve;
-
-	createAchievement({
-		mod: BCAchieve,
-		name: "Test Achivement",
-		description: "Test the achivement system."
-	});
-
-	createAchievement({
-		mod: BCAchieve,
-		name: "Test Incremental Achivement",
-		description: "Test the achivement system.",
-		amount: 5
-	});
-
-	createAchievement({
-		mod: BCAchieve,
-		name: "Test Image Achivement",
-		description: "Test the achivement system.",
-		icon: "https://boxcritters.com/images/menu/Btn_Menu_Items_A.png"
-	});
 })();
