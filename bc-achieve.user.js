87b8e6b7dcbae68ccd3baa3d63caaa46375b59a4
tumble <tumblegamer@gmail.com>
2022-05-06 00:28:37 +0100

    reound values

--- bc-achieve.user.js
+++ bc-achieve.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BCAchieve
 // @namespace    https://bcmc.ga/authors/tumble/
-// @version      0.1.2.3
+// @version      0.1.3.4
 // @author       Tumble
 // @require      https://github.com/tumble1999/mod-utils/raw/master/mod-utils.js
 // @require      https://github.com/tumble1999/modial/raw/master/modial.js
@@ -66,10 +66,10 @@
 		achievementListPage = Critterguration.registerSettingsMenu(BCAchieve, () => {
 			achievementList.querySelectorAll(".list-group-item").forEach(i => i.remove());
 
-			tasks.sort((a, b) => (completion[b.id] || 0) / b.amount - (completion[a.id] || 0) / a.amount).forEach(task => {
+			tasks.sort((a, b) => b.completion() / b.amount - a.completion() / a.amount).forEach(task => {
 
 				let status = "secondary";
-				if (!!completion[task.id]) status = "primary";
+				if (!!task.completion()) status = "primary";
 				if (task.achieved) status = "success";
 				if (task.hidden && status != "success") return;
 				achievementList.addItem({
@@ -77,9 +77,9 @@
 					color: status,
 					description: task.description,
 					footer: task.author || task.mod.name,
-					corner: (completion[task.id] || 0) + "/" + task.amount,
-					badge: completion[task.id] == task.amount ? "Done" : null,
-					active: !!completion[task.id]
+					corner: Math.round(task.completion()) + "/" + task.amount,
+					badge: task.completion() == task.amount ? "Done" : null,
+					active: !!task.completion()
 				});
 
 			});
@@ -91,34 +91,35 @@
 		if (!task.amount) task.amount = 1;
 		tasks.push(task);
 
+		task.completion = function () {
+			return completion[task.id] || 0;
+		};
+
 		// Automatically mark as achived after created
-		if (completion[task.id] >= task.amount) task.achieved = true;
+		if (task.completion() >= task.amount) task.achieved = true;
 
 		task.achieve = function (amount = 1) {
-			if (typeof completion[task.id] != "number") completion[task.id] = 0;
-			completion[task.id] += amount;
+			if (typeof task.completion() != "number") completion[task.id] = 0;
+			if (!task.achieved || task.completion() == 1) completion[task.id] += amount;
 			// save any potential changes
 			save();
 
-			//if (completion[task.id] > task.amount) completion[task.id] = task.amount;
-			if (!task.achieved && completion[task.id] >= task.amount) {
+			//if (task.amount) completion[task.id] = task.amount;
+			if (!task.achieved && task.completion() >= task.amount) {
 				// Mark as achieved when completed
 				task.achieved = true;
 				BCNotify.notify({
 					mod: BCAchieve,
-					title: task.name + (task.amount == 1 ? "" : " (" + completion[task.id] + "/" + task.amount + ")"),
+					title: task.name + (task.amount == 1 ? "" : " (" + math.round(task.completion()) + "/" + task.amount + ")"),
 					body: task.description,
 					icon: task.icon
 				});
 			}
 		};
 		task.set = function (amount) {
-			task.achieve(amount - completion[task.id]);
+			task.achieve(amount - task.completion());
 		};
 
-		task.completion = function () {
-			return completion[task.id];
-		};
 
 		return task;
 	}
